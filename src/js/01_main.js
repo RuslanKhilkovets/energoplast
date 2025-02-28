db06473e84033917464c26c856c8ecf9491ac8cb
RX <ruslanxilkovec@gmail.com>
2025-02-28 16:08:46 +0200

    clean project / added base

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -4,13 +4,8 @@ const openModalButton = document.querySelector(".open-modal");
 const modal = document.querySelector(".modal");
 
 const handleBurgerMenuOpen = () => {
-    burger.classList.toggle("burger-active");
-    navbar.classList.toggle("mobile-navbar--active")
-}
+  burger.classList.toggle("burger-active");
+  navbar.classList.toggle("mobile-navbar--active");
+};
 
-const handleModal = () => {
-    modal.classList.toggle("modal--active")
-}
-
-openModalButton.addEventListener("click",  handleModal)
-burger.addEventListener("click", handleBurgerMenuOpen)
+burger.addEventListener("click", handleBurgerMenuOpen);
