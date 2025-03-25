7c7527fcf887911ef9665d36b5b43f3c8630a09e
Ruslan_Khilkovets <ruslanxilkovec@gmail.com>
2025-03-25 11:10:13 +0200

    .

--- tasks/html.js
+++ tasks/html.js
@@ -1,13 +1,21 @@
-const {
-	src,
-	dest
-} = require('gulp');
-const include = require('gulp-file-include');
-const bs = require('browser-sync');
+const { src, dest } = require("gulp");
+const include = require("gulp-file-include");
+const bs = require("browser-sync");
+const plumber = require("gulp-plumber");
 
 module.exports = function html() {
-	return src(['src/**/*.html', '!src/components/**/*.html'])
-		.pipe(include())
-		.pipe(dest('build'))
-		.pipe(bs.stream())
-}
+  return src(["src/**/*.html", "!src/components/**/*.html"])
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          // Log the error with the file name
+          console.error(`Error in file: ${err.fileName}`);
+          console.error(err.message);
+          this.emit("end"); // Continue processing other files after an error
+        },
+      })
+    )
+    .pipe(include())
+    .pipe(dest("build"))
+    .pipe(bs.stream());
+};
